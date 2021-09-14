8765432d7a7cc4b0b30807a8bcfa48bcaa103563
Dominic Barajas <dominicbarajas548@gmail.com>
2021-09-13 20:38:08 -0600

    video test
    embed video from youtube test

--- apex-frontend/src/components/About.js
+++ apex-frontend/src/components/About.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import "../styles.css";
+import YoutubeEmbed from "./YoutubeEmbed";
 
 export const About = () => {
     return (
         <div>
             <h1>ABOUT</h1>
+
+            <iframe width="864" height="486" src="https://www.youtube.com/embed/aNXM6m7dPfk?list=PLDFBC23BD531B290F" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
     )
 }
