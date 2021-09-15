e93ff66dfbeb8685f6e55a94e32a498f63f6dea9
Dominic Barajas <dominicbarajas548@gmail.com>
2021-09-15 14:01:18 -0600

    test YoutubeEmbed component
    testing the YoutubeEmbed component to place embeded video on page

--- apex-frontend/src/components/About.js
+++ apex-frontend/src/components/About.js
@@ -6,6 +6,7 @@ export const About = () => {
     return (
         <div>
             <h1>ABOUT</h1>
+            <YoutubeEmbed embedId="aNXM6m7dPfk" />
 
             <iframe width="864" height="486" src="https://www.youtube.com/embed/aNXM6m7dPfk?list=PLDFBC23BD531B290F" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
