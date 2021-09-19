77ed611e84cb0ace45d3a8a55dcbf6e2b555351e
Dominic Barajas <dominicbarajas548@gmail.com>
2021-09-18 19:48:57 -0600

    Update VideoLibrary.js
    add rest of mobility nav links and progression nav links

--- apex-frontend/src/components/VideoLibrary.js
+++ apex-frontend/src/components/VideoLibrary.js
@@ -13,7 +13,7 @@ const VideoNavigation = () => {
     return (
         <div className='navlink'>
 
-            {/* mobility */}
+{/* mobility */}
             <NavLink
                 to='/spine_mobility'
                 exact
@@ -49,15 +49,55 @@ const VideoNavigation = () => {
                     background: "#191d32",
                     color: 'white'
                 }}>Bridge Training</NavLink>
-{/* strength */}
-            <NavLink
-                to='/drop_land'
+
+<NavLink
+                to='/lower_body_mob'
                 exact
                 style={link}
                 activeStyle={{
                     background: "#191d32",
                     color: 'white'
-                }}>Drop/Landing</NavLink>
+                }}>Lower Body Mobility</NavLink>
+
+<NavLink
+                to='/ankle_mob'
+                exact
+                style={link}
+                activeStyle={{
+                    background: "#191d32",
+                    color: 'white'
+                }}>Ankle mobility</NavLink>
+
+
+{/* UPPER BODY */}
+            <NavLink
+                to='/pushing_movements'
+                exact
+                style={link}
+                activeStyle={{
+                    background: "#191d32",
+                    color: 'white'
+                }}>Pushing</NavLink>
+
+            <NavLink
+                to='/pulling_movements'
+                exact
+                style={link}
+                activeStyle={{
+                    background: "#191d32",
+                    color: 'white'
+                }}>Pulling</NavLink>
+{/* LOWER BODY */}
+
+            <NavLink
+                to='/lowerbody'
+                exact
+                style={link}
+                activeStyle={{
+                    background: "#191d32",
+                    color: 'white'
+                }}>Lower body</NavLink>
+
 {/* progressions and drills */}
             <NavLink
                 to='/brachiate'
@@ -86,7 +126,97 @@ const VideoNavigation = () => {
                     color: 'white'
                 }}>Run/ Sprint training</NavLink>
 
-                {/* PROGRAMS */}
+            <NavLink
+                to='/climbup'
+                exact
+                style={link}
+                activeStyle={{
+                    background: "#191d32",
+                    color: 'white'
+                }}>Climb-up</NavLink>
+
+            <NavLink
+                to='/quadrupedal'
+                exact
+                style={link}
+                activeStyle={{
+                    background: "#191d32",
+                    color: 'white'
+                }}>Quadrupedal Movements</NavLink>
+
+<NavLink
+                to='/weightlifting'
+                exact
+                style={link}
+                activeStyle={{
+                    background: "#191d32",
+                    color: 'white'
+                }}>Weightlifting</NavLink>
+
+<NavLink
+                to='/muscleup'
+                exact
+                style={link}
+                activeStyle={{
+                    background: "#191d32",
+                    color: 'white'
+                }}>Muscle Up</NavLink>
+
+<NavLink
+                to='/rails'
+                exact
+                style={link}
+                activeStyle={{
+                    background: "#191d32",
+                    color: 'white'
+                }}>Rail balance</NavLink>
+
+<NavLink
+                to='/jump_land'
+                exact
+                style={link}
+                activeStyle={{
+                    background: "#191d32",
+                    color: 'white'
+                }}>jump/Land training</NavLink>
+
+<NavLink
+                to='/squat'
+                exact
+                style={link}
+                activeStyle={{
+                    background: "#191d32",
+                    color: 'white'
+                }}>Squats</NavLink>
+
+<NavLink
+                to='/hanging'
+                exact
+                style={link}
+                activeStyle={{
+                    background: "#191d32",
+                    color: 'white'
+                }}>hanging</NavLink>
+
+<NavLink
+                to='/single_leg'
+                exact
+                style={link}
+                activeStyle={{
+                    background: "#191d32",
+                    color: 'white'
+                }}>Single Leg squats</NavLink>
+
+{/* PROGRAMS */}
+
+            <NavLink
+                to='pkstrength/untrained'
+                exact
+                style={link}
+                activeStyle={{
+                    background: "#191d32",
+                    color: 'white'
+                }}>Untrained Beginner</NavLink>
 
                 <NavLink
                 to='pkstrength/beginner'
@@ -114,6 +244,24 @@ const VideoNavigation = () => {
                     background: "#191d32",
                     color: 'white'
                 }}>Advanced Movements</NavLink>
+
+            <NavLink
+                to='/parkour_freerunning'
+                exact
+                style={link}
+                activeStyle={{
+                    background: "#191d32",
+                    color: 'white'
+                }}>parkour / freerunning tutorials(beginner/intermediate)</NavLink>
+
+            <NavLink
+                to='/handstand'
+                exact
+                style={link}
+                activeStyle={{
+                    background: "#191d32",
+                    color: 'white'
+                }}>Handstand</NavLink>
 
         </div>
     )
